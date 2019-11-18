f26b9375f740323474034ca3ffca2a45de60c3f3
hoang <hoang@192.168.134.1>
2019-11-18 22:19:44 +0700

    Dang nhap, Quan Ly fix SanPham Test

---
+++ src/edu/poly/dao/SanPhamDAO.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.poly.dao;
+
+
+import edu.poly.helper.JdbcHelper;
+import edu.poly.model.SanPham;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SanPhamDAO {
+
+    public void insert(SanPham model) {
+        String sql = "INSERT INTO SanPham (masp, tensp, giasp, soluongsp, hinhsp, maloai,maql) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        JdbcHelper.executeUpdate(sql, model.getTensp(), model.getGiasp(), model.getSoluongsp(), model.getHinhsp(), model.getMaloai(),model.getMaql());
+    }
+
+    public void update(SanPham model) {
+        String sql = "UPDATE SanPham SET tensp=?, giasp=?, soluongsp=?, hinhsp=?,maloai=? maql=? WHERE masp=?";
+        JdbcHelper.executeUpdate(sql, model.getTensp(), model.getGiasp(), model.getSoluongsp(), model.getHinhsp(), model.getMaloai(),model.getMaql());
+    }
+
+    public void delete(String masp) {
+        String sql = "DELETE FROM SanPham WHERE masp=?";
+        JdbcHelper.executeUpdate(sql, masp);
+    }
+
+    public List<SanPham> select() {
+        String sql = "SELECT * FROM SanPham";
+        return select(sql);
+    }
+
+    public SanPham findById(String macd) {
+        String sql = "SELECT * FROM SanPham WHERE masp=?";
+        List<SanPham> list = select(sql, macd);
+        return list.size() > 0 ? list.get(0) : null;
+    }
+
+    private List<SanPham> select(String sql, Object... args) {
+        List<SanPham> list = new ArrayList<>();
+        try {
+            ResultSet rs = null;
+            try {
+                rs = JdbcHelper.executeQuery(sql, args);
+                while (rs.next()) {
+                    SanPham model = readFromResultSet(rs);
+                    list.add(model);
+                }
+            } finally {
+                rs.getStatement().getConnection().close();
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+        return list;
+    }
+
+    private SanPham readFromResultSet(ResultSet rs) throws SQLException {
+        SanPham model = new SanPham();
+        model.setMasp(rs.getString("masp"));
+        model.setTensp(rs.getString("Hinh"));
+        model.setGiasp(rs.getFloat("giasp"));
+        model.setSoluongsp(rs.getInt("soluongsp"));
+        model.setHinhsp(rs.getString("hinhsp"));
+        model.setMaloai(rs.getString("maloai"));
+        model.setMaql(rs.getString("maql"));
+        return model;
+    }
+}
+
