32c6d2b999dbccc0754365aad0d3f71c669e5234
hoang <hoang@192.168.134.1>
2019-11-19 13:26:12 +0700

    Dang nhap, Quan Ly fix

--- src/edu/poly/view/SanPhamJFrame.java
+++ src/edu/poly/view/SanPhamJFrame.java
@@ -162,7 +162,6 @@ public class SanPhamJFrame extends javax.swing.JFrame {
             }
         }
     }
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
